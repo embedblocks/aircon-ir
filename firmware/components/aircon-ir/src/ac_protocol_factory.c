7b8179307285ec5f2fdfd3033171d6699c04b353
khiyam <khiyamiftikhar@gmail.com>
2026-08-17 12:39:22 +0500

    feat: condiontally includes brand specific src based on Kconfig

--- firmware/components/aircon-ir/src/ac_protocol_factory.c
+++ firmware/components/aircon-ir/src/ac_protocol_factory.c
@@ -1,10 +1,17 @@
 #include "ac_protocol.h"
-
 #include "protocols/midea.h"
-
-
+#include "protocols/gree.h"
+#include "protocols/haier.h"
 
 const ac_protocol_t *ac_protocol_get(void)
 {
+#if CONFIG_AC_PROTOCOL_MIDEA
     return &midea_protocol;
+#elif CONFIG_AC_PROTOCOL_GREE
+    return &gree_protocol;
+#elif CONFIG_AC_PROTOCOL_HAIER
+    return &haier_protocol;
+#else
+#error "No AC protocol selected"
+#endif
 }
